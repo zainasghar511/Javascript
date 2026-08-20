b9254a48699cff0579a556f17504d5b2a1558808
zainasghar511 <zainasghae6@gmail.com>
2026-08-20 23:31:29 +0500

    calcultar

--- java.js
+++ java.js
@@ -1,17 +1,36 @@
-const inputdata = document.querySelector(".input");
-const span = document.querySelector(".span");
+// const inputdata = document.querySelector(".input");
+// const span = document.querySelector(".span");
 
 
-inputdata.addEventListener('input',function (e) {
-    const typetext = e.target.value;
-    span.textContent = typetext
-    if(typetext.length>10){
-          span.textContent = typetext
-    }else if(typetext.length<10){
-        console.log("high")
-    }
-    else{
-        console.log("low")
-    }
-})
+// inputdata.addEventListener('input',function (e) {
+//     const typetext = e.target.value;
+//     span.textContent = typetext
+//     if(typetext.length<10){
+//           span.textContent = typetext.length
+//     }else if(typetext.length>10){
+//         console.log("high")
+//     }
+//     else{
+//         console.log("low")
+//     }
+// })
+let random = Math.random()
+let a = prompt("Enter first Number")
 
+let b = prompt("Enter operation")
+let c = prompt("Enter second Number")
+let obj = {
+    "+":"-",
+    "*":"+",
+    "-":"/",
+    "/":"//"
+}
+
+if (random>0.1) {
+    alert(`Your result is ${eval(`${a} ${b} ${c}`)}`)
+    
+} else {
+    c=obj[c]
+   alert(`Your result is ${eval(`${a} ${b} ${c}`)}`)
+   
+}

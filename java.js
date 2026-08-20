b0b2f31439973538dddf92ad814a49fb54059fd3
zainasghar511 <zainasghae6@gmail.com>
2026-08-20 23:11:05 +0500

    add data

--- java.js
+++ java.js
@@ -1,5 +1,17 @@
-function name(params) {
-    console.log("Hello"+ params)
-}
-name("zain")
-name("Hamza")
+const inputdata = document.querySelector(".input");
+const span = document.querySelector(".span");
+
+
+inputdata.addEventListener('input',function (e) {
+    const typetext = e.target.value;
+    span.textContent = typetext
+    if(typetext.length>10){
+          span.textContent = typetext
+    }else if(typetext.length<10){
+        console.log("high")
+    }
+    else{
+        console.log("low")
+    }
+})
+

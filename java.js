ee81c0dcb2d8f51514eecdd5349f784eeec8b2aa
zainasghar511 <zainasghae6@gmail.com>
2026-08-24 23:09:34 +0500

    make

--- java.js
+++ java.js
@@ -1,1 +1,11 @@
-const value = [1,2,3,4,]
+let b = 6
+function Factorial(number) {
+    let arr = Array.from(Array(number+1).keys())
+    console.log(arr.slice(1))
+   let c=  arr.slice(1).reduce((h1,h2)=>{
+return h1*h2
+    })
+    console.log(c)
+}
+
+Factorial(b)

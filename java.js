8ce81f7219b281aef27d3241771f0914353d6f69
zainasghar511 <zainasghae6@gmail.com>
2026-08-29 00:41:43 +0500

    learn about time

--- java.js
+++ java.js
@@ -1,11 +1,18 @@
-let view = document.querySelector(".view");
-let data = document.querySelector(".data");
-let btn1 = document.querySelector(".click");
-let check = document.querySelector(".check");
 
-btn1.addEventListener("dblclick",()=>{
-view.innerHTML = Date()
+
+let press = document.querySelector(".press")
+console.log(press)
+
+press.addEventListener('click',data=()=>{
+    console.log("you are  clicking me")
+  const para =document.createElement("p");
+  para.textContent = "Thanks for pressing me"
+  document.body.appendChild(para)
+    setTimeout(() => {
+         const love = document.createElement("h3")
+    love.textContent = "I Love You"
+    document.body.appendChild(love)
+        
+    }, 2000);
+  
 })
-setInterval(() => {
-    data.innerHTML  += "you are made"
-}, 2000);

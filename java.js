b27c7024be59b967879c7502053cf7c6814ba420
zainasghar511 <zainasghae6@gmail.com>
2026-08-29 01:13:48 +0500

    promise callback

--- java.js
+++ java.js
@@ -16,3 +16,39 @@ press.addEventListener('click',data=()=>{
     }, 2000);
   
 })
+
+
+
+const number = [12,3,5,7,89,99]
+const double = number.map(number=>number*2)
+console.log(double)
+const f = number.filter(number=>number%2===0)
+console.log(f)
+function data(wait) {
+    console.log("ks cheez ka wait kr rhy ho")
+    setTimeout(() => {
+        const name = "hurry i am here what are you searching"
+        wait(name)
+    }, 2000);
+}
+function mill(ok) {
+    console.log("shukr hai data mill gya", ok)
+}
+data(mill)
+const datapromise = new Promise((resolve,rejected)=>{
+    console.log("just wait maybe i will come")
+    setTimeout(() => {
+        const oh = true;
+        if (oh) {
+            resolve({name:"zain",RollNumber:45, course :"computer science"})
+            
+        }else{
+            rejected("we are fatching some eror")
+        }
+    }, 3000);
+})
+datapromise.then((maybe)=>{
+    console.log("mill tu gya hai data",maybe)
+}).catch((erormsg)=>{
+    console.log("ya umeed nai the tm sy",erormsg)
+})

8f14e1513bf9a55e815b0cc3ee4ca9d642d44936
zainasghar511 <zainasghae6@gmail.com>
2026-08-30 23:55:18 +0500

    mini

---
+++ javaa.js
@@ -0,0 +1,16 @@
+function graph(params) {
+    
+    setTimeout (()=>{const heading1 = document.createElement("H1")
+heading1.textContent = "It will be risk thank"
+heading1.style.color = "Red"
+document.body.appendChild(heading1)
+},3000)
+
+setTimeout( () => {
+ const heading1 = document.createElement("H1")
+heading1.textContent = "It will be risk thank"
+heading1.style.color = "Red"
+document.body.appendChild(heading1)
+
+}, 2000);}
+graph()
